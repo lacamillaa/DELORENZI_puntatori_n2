e52a424ce1c7e44eaa93ac44e80679f95821b5ee
lacamillaa <12999356@itisrossi.vi.it>
2025-10-15 11:37:19 +0200

    duplicate function

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char* duplicate(char* s) {
+    int i = 0;
+    while (s[i] != '\0') {
+        i++;
+    }
+    char* dbl = (char*)malloc(i + 1);
+    if (dbl == NULL) return NULL;
+    for (int j = 0; j < i; j++) {
+        dbl[j] = s[j];
+    }
+    dbl[i] = 0;
+    free(s);
+    return dbl;
+}
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
 }
